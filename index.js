6b233327c4583bd0467c6f19fc694d9f691d9af0
Brad Deniger <62615814+nexx74@users.noreply.github.com>
2020-09-25 16:08:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1297,10 +1297,9 @@ for (l = 0; l < 10; l++) {
 		spam[l] = spam[l] + rand();
 	}
 }
-	if (message.content.startsWith === '/nux-y') {
+	if (message.content.startsWith('/nux-y')) {
 		for (c = 0; c < 50; c++) {
 			message.channel.send(spam[c])
-			console.log(`${client.user.tag} ran command </nux>!!!!`);
 			
 		}
 	}
@@ -1326,7 +1325,6 @@ if (message.content.startsWith(prefix + "nuk-y")) {
     var challenged = user.toString();
 
     message.channel.send(`> ** ${author1} nuk started** `)
-    console.log(`${client.user.tag} ran command </nuk-y>!!!!`)
 
     message.channel.send('```BOOM```')
         .then(() => {
