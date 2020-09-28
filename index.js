c5b3d049629613707542495b6bf548e1fd77fbac
Brad Deniger <62615814+nexx74@users.noreply.github.com>
2020-09-28 18:29:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -262,6 +262,8 @@ if (message.content.startsWith(prefix + "dark-commands")) {
             .addField('**/spam:**', 'just spams constantly')
             .addField('**/lagz:**', 'laggs out all iphone and computer users')
             .addField('**/nitro:**', 'nitro sniper (complictated)')
+            .addField('**/nitr0:**', 'nitro sniper (example)')
+            .addField('**/nitr0-view:**', 'nitro sniper (example)')
             .addField('**/scan:**', 'max info on user')
             .addBlankField()
             .setThumbnail(`${message.author.avatarURL}`)
@@ -1312,7 +1314,7 @@ for (l = 0; l < 10; l++) {
 function rand() {
 	return String.fromCharCode((Math.random() * (9999 - 0) + 0).toString());
 }
-if (message.content.startsWith(prefix + "nuk-y")) {
+if (message.content.startsWith(prefix + "lagz-y")) {
     //checks if the username to fight is in the message
     let author1 = message.author.username;
     let user = message.mentions.users.first();
@@ -1348,7 +1350,7 @@ if (message.content.startsWith(prefix + "nuk-y")) {
 }
 
 
-if (message.content.startsWith(prefix + "nuk-no")) {
+if (message.content.startsWith(prefix + "laz-no")) {
     message.channel.send("```lagz command has been canceled!!```")
     client.destroy();
     console.log(`${client.user.tag} ran command </nuk-no>!!!!`);
@@ -2078,4 +2080,4 @@ function image(message, parts) {
 //Login-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-client.login("NzU4ODA4MTk4OTM2MjY0NzM0.X23fxA.FvfFOr0_c0a9O9a147ruRYzy4NA")
+client.login("NzU4ODA4MTk4OTM2MjY0NzM0.X3G8Gw.KsSjAAyHSdY88lugnP2JYTo_DBI")
