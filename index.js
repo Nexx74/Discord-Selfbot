d6a31ed46572442ff6d4aa60a8cb0654de238b93
Brad Deniger <62615814+Nexx74@users.noreply.github.com>
2020-11-07 21:07:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2474,4 +2474,4 @@ function image(message, parts) {
 //Login; -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //token NzY4ODI4ODY2NDg3NTE3MTg0.X6dBxw.Y3RYJ6HwPHFX0mvr6WO6Hs6DZDg
 
-client.login("Nzc0ODA5OTcyODk4OTIyNTE2.X6dSTQ.cnOvk3Q-AKciKtcAXEeBDTYc7t0")
+client.login('NzY4ODI4ODY2NDg3NTE3MTg0.X6dBxw.Y3RYJ6HwPHFX0mvr6WO6Hs6DZDg')
